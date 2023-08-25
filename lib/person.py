413c0b5f2278774530235803cd37fbd1a73ccfd7
Nigel <nigel.oketch@student.moringaschool.com>
2023-08-25 09:32:27 +0300

    complete

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 class Person:
+      
     # Class body goes here
+        def talk (self):
+              print ("Hello World!")
 
+nigel = Person()
+nigel.talk()
+   
     #Instance method definition
-    pass
+    
